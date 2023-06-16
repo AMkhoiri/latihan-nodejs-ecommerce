082d570e3a5aa70400135c33f96f4cc4ef5fe359
ahmad <ahmad.m.khoiri@gmail.com>
2023-06-16 07:20:13 +0700

    landing page for testing AWS EC2

---
+++ web.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import path from "path"
+import { fileURLToPath } from 'url'
+
+const app = express()
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+app.get("/landing-page", (req, res) => {
+  	res.setHeader('Content-Type', 'text/html').sendFile('./landing-page.html', {root: __dirname})
+})
+
+
+app.listen(3001, () => console.log('Server started on port 3001'))
